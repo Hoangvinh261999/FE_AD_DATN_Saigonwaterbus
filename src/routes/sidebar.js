7afbb4975491f7993ecf34bab769f68ca0f6ecc5
Nguyễn Hoàng Vinh <96643414+Hoangvinh261999@users.noreply.github.com>
2024-06-11 09:21:38 +0700

    Sua thong tin leftbar

--- src/routes/sidebar.js
+++ src/routes/sidebar.js
@@ -27,43 +27,94 @@ const routes = [
   {
     path: '/app/dashboard',
     icon: <Squares2X2Icon className={iconClasses}/>, 
-    name: 'Dashboard',
+    name: 'Trang chủ',
   },
   {
     path: '/app/leads', // url
     icon: <InboxArrowDownIcon className={iconClasses}/>, // icon component
-    name: 'Leads', // name that appear in Sidebar
+    name: 'Tuyến', // name that appear in Sidebar
   },
   {
     path: '/app/transactions', // url
     icon: <CurrencyDollarIcon className={iconClasses}/>, // icon component
-    name: 'Transactions', // name that appear in Sidebar
+    name: 'Chuyến', // name that appear in Sidebar
   },
   {
     path: '/app/charts', // url
     icon: <ChartBarIcon className={iconClasses}/>, // icon component
-    name: 'Analytics', // name that appear in Sidebar
+    name: 'Bến', // name that appear in Sidebar
   },
   {
     path: '/app/integration', // url
     icon: <BoltIcon className={iconClasses}/>, // icon component
-    name: 'Integration', // name that appear in Sidebar
+    name: 'Vé', // name that appear in Sidebar
   },
   {
     path: '/app/calendar', // url
     icon: <CalendarDaysIcon className={iconClasses}/>, // icon component
-    name: 'Calendar', // name that appear in Sidebar
+    name: 'Ghế', // name that appear in Sidebar
   },
+
 
   {
     path: '', //no url needed as this has submenu
+    icon: <Cog6ToothIcon className={`${iconClasses} inline` }/>, // icon component
+    name: 'Hoá đơn', // name that appear in Sidebar
+    submenu : [
+      {
+        path: '/app/settings-profile', //url
+        icon: <UserIcon className={submenuIconClasses}/>, // icon component
+        name: 'Profile', // name that appear in Sidebar
+      },
+      {
+        path: '/app/settings-billing',
+        icon: <WalletIcon className={submenuIconClasses}/>,
+        name: 'Billing',
+      },
+      {
+        path: '/app/settings-team', // url
+        icon: <UsersIcon className={submenuIconClasses}/>, // icon component
+        name: 'Team Members', // name that appear in Sidebar
+      },
+    ]
+  },
+  {
+    path: '', //no url needed as this has submenu
+    icon: <DocumentTextIcon className={`${iconClasses} inline` }/>, // icon component
+    name: 'Nhân viên', // name that appear in Sidebar
+    submenu : [
+      {
+        path: '/app/getting-started', // url
+        icon: <DocumentTextIcon className={submenuIconClasses}/>, // icon component
+        name: 'Thuyền Trưởng', // name that appear in Sidebar
+      },
+      {
+        path: '/app/features',
+        icon: <TableCellsIcon className={submenuIconClasses}/>, 
+        name: 'Nhân viên trong bến',
+      },
+      {
+        path: '/app/components',
+        icon: <CodeBracketSquareIcon className={submenuIconClasses}/>, 
+        name: 'Components',
+      }
+    ]
+  },
+  
+  {
+    path: '', //no url needed as this has submenu
     icon: <DocumentDuplicateIcon className={`${iconClasses} inline` }/>, // icon component
-    name: 'Pages', // name that appear in Sidebar
+    name: 'Trang', // name that appear in Sidebar
     submenu : [
       {
         path: '/login',
         icon: <ArrowRightOnRectangleIcon className={submenuIconClasses}/>,
-        name: 'Login',
+        name: 'Đăng nhập',
+      },
+            {
+        path: '/login',
+        icon: <ArrowRightOnRectangleIcon className={submenuIconClasses}/>,
+        name: 'Đăng xuất',
       },
       {
         path: '/register', //url
@@ -87,51 +138,6 @@ const routes = [
       },
     ]
   },
-  {
-    path: '', //no url needed as this has submenu
-    icon: <Cog6ToothIcon className={`${iconClasses} inline` }/>, // icon component
-    name: 'Settings', // name that appear in Sidebar
-    submenu : [
-      {
-        path: '/app/settings-profile', //url
-        icon: <UserIcon className={submenuIconClasses}/>, // icon component
-        name: 'Profile', // name that appear in Sidebar
-      },
-      {
-        path: '/app/settings-billing',
-        icon: <WalletIcon className={submenuIconClasses}/>,
-        name: 'Billing',
-      },
-      {
-        path: '/app/settings-team', // url
-        icon: <UsersIcon className={submenuIconClasses}/>, // icon component
-        name: 'Team Members', // name that appear in Sidebar
-      },
-    ]
-  },
-  {
-    path: '', //no url needed as this has submenu
-    icon: <DocumentTextIcon className={`${iconClasses} inline` }/>, // icon component
-    name: 'Documentation', // name that appear in Sidebar
-    submenu : [
-      {
-        path: '/app/getting-started', // url
-        icon: <DocumentTextIcon className={submenuIconClasses}/>, // icon component
-        name: 'Getting Started', // name that appear in Sidebar
-      },
-      {
-        path: '/app/features',
-        icon: <TableCellsIcon className={submenuIconClasses}/>, 
-        name: 'Features',
-      },
-      {
-        path: '/app/components',
-        icon: <CodeBracketSquareIcon className={submenuIconClasses}/>, 
-        name: 'Components',
-      }
-    ]
-  },
-  
 ]
 
 export default routes
